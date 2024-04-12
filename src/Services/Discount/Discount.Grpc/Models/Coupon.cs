dcc3d7df7661b8d7e8fa8785425dd2cce00fc3b7
DeltaMonkey <engincanozkan@outlook.com>
2024-04-13 00:37:03 +0300

    Discount.Grpc Microservices for Microservices Grpc Communication

---
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,9 @@
+﻿namespace Discount.Grpc.Models;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; } = default!; //TODO: Can I change it to ID
+    public string Description { get; set; } = default!;
+    public int Amount { get; set; }
+}
