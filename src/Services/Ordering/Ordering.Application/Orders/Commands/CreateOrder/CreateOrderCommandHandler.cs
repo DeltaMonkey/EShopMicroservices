dea411d6c3ad475b93243ed11947eca4bb749077
DeltaMonkey <engincanozkan@outlook.com>
2024-05-30 22:40:30 +0300

    Ordering.Application Layer with CQRS and MediatR _STEP_1: CreateOrderCommand classes are created

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, CreateOrderCommandResult>
+{
+    public Task<CreateOrderCommandResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        //create Order entity from command object
+        //save to database
+        //return result
+
+        throw new NotImplementedException();
+    }
+}
